84f412035c8381467c8c3a050a66bbbe819c29b0
Andrei L <andrewlopukhov@gmail.com>
2021-04-10 18:10:20 +0300

    Deploy website - based on 41ffd03136891b346aed0224cd4e06182d32b344

---
+++ assets/js/ae8bff84.9499ce21.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{83:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Hello world.","permalink":"/blog/2021/04/10/starting"}]}')}}]);
